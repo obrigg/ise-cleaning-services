501332ad1f981007e49641996bf01e0655944e53
Oren Brigg <obrigg@cisco.com>
2021-04-10 13:32:10 +0300

    Changes the group selection to tag-based #cleanup

--- run.py
+++ run.py
@@ -8,13 +8,6 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
-######           User Inputs        ######
-list_of_group_names = [ "AAA-Vouchers",
-                        "BBB-Vouchers",
-                        "Vitorcam-IPCamera",
-                        "PartnerTV-Android-Streamer"
-                    ]
-
 ######   Setting up the environment ######
 ise_user = os.environ.get('ISE_USER', "admin")
 ise_password = os.environ.get('ISE_PASSWORD', "")
@@ -25,19 +18,25 @@ headers = {"Content-Type": "application/json",
            "Accept": "application/json"}
 
 
-def get_ise_group_id(group_name: str):
+def get_ise_cleanup_groups():
     '''
-    This function will return the ISE group id a given group name.
+    This function will search ISE for endpoint groups that include the
+    tag #cleanup in their description for the cleanup process.
+    It will return a list of group IDs.
     '''
-    print(f"Fetching for ISE for endpoint group {group_name}...")
-    url = base_url + "endpointgroup/name/" + group_name
+    print("Fetching ISE's endpoint groups...")
+    url = base_url + "endpointgroup?size=100"
     response = requests.get(url=url, auth=auth, headers=headers, verify=False)
     if response.status_code == 200:
-        group_id = response.json()['EndPointGroup']['id']
-        print(f"ISE endpoint group {group_name}, id: {group_id}")
-        return(group_id)
+        cleanup_groups = []
+        groups = response.json()['SearchResult']['resources']
+        for group in groups:
+            if "#cleanup" in group['description'].lower():
+                print(f"Endpoint group {group['name']} will be cleaned up")
+                cleanup_groups.append(group['id'])
+        return(cleanup_groups)
     else:
-        print(f"ERROR: Group {group_name} was not found")
+        print(f"ERROR: {response.text}")
         return("ERROR")
 
 
@@ -82,14 +81,12 @@ def delete_endpoint(endpoint_id: str):
     if response.status_code != 204:
         print(f"Error deleting endpoint {endpoint_id}: {response.text}")
 
-# Translate group names to group IDs
-list_of_group_ids = []
-for group_name in list_of_group_names:
-    groupId = get_ise_group_id(group_name)
-    list_of_group_ids.append(groupId)
+
+# Gather relevant cleanup groups
+cleanup_groups = get_ise_cleanup_groups()
 # Gather all relevant endpoints
 list_of_endpoints = []
-for groupId in list_of_group_ids:
+for groupId in cleanup_groups:
     endpoints = get_endpoints_by_group_id(groupId)
     if endpoints != None:
         list_of_endpoints += endpoints
